8db6dbb0eca74660f994c46856479610c34ba615
krymzach <krymgz00@icloud.com>
2022-03-09 21:30:23 -0500

    Added ability to add a second node

--- app.js
+++ app.js
@@ -4,3 +4,12 @@ import { firebaseAPIConfig } from "./env.js";
 
 // Initialize Firebase
 const app = initializeApp(firebaseAPIConfig);
+
+const app_page = document.getElementById('app_page');
+const button1 = document.getElementById('1vu');
+
+button1.onclick = function () { createNode('f', '0, 0', 1, 'Patricia Krymgold'); };
+
+function createNode(gender, prevNodePos, prevId, name) {
+    app_page.innerHTML += "<div class='node_container' style='left: 50%; top: calc(50% - 10vw); transform: translate(-50%, -50%);' id=" + String(prevId + 1) + "> <div class='vertical_line' id=" + String(prevId + 1) + "vu'> <div class='add_icon'> <p class='add'>+</p> </div> </div> <div class='node " + gender + "'> <!--<img src='' alt=''>--> <p class='no_image'>?</p> </div> <p class='node_name'>" + name + "</p> </div>";
+}
